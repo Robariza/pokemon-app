73d1fb6a6e0e9f3d5f9d4abc6f7b7ab6eab58056
Robariza <robinsonariza2001@gmail.com>
2024-11-16 17:37:28 -0500

    [update] routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';  // Se importa el componente para mostrar la lista de Pokémon.
+import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';  // Se importa el componente para mostrar los detalles de un Pokémon específico.
 
-export const routes: Routes = [];
+export const routes: Routes = [  // Definimos un arreglo de rutas que sigue la estructura 'Routes' proporcionada por Angular.
+  { path: '', component: PokemonListComponent },  // Ruta raíz ('') que muestra la lista de Pokémon cuando no hay una ruta específica.
+  { path: 'pokemon/:id', component: PokemonDetailComponent },  // Ruta dinámica 'pokemon/:id' que carga el componente de detalles del Pokémon.
+];
